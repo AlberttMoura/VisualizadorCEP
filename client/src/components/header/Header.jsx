742f2264fd6e6feccd6cc02d07ff36a5b523e41d
AlberttMoura <BetinhoolXD@gmail.com>
2021-11-14 10:02:48 -0300

    Making a Header

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="brand-side">
+                <p className="brand-name">CEPZêra</p>
+                <p className="brand-slogan">Consulte CEPs de todo o Brasil</p>
+            </div>
+        </div>
+    )
+}
+
+export default Header
